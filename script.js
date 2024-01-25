520d14c5da06b704debc10b29b2a0d4f739d612b
UirleandroSantos <uirleandro.santos.19@gmail.com>
2024-01-25 15:06:01 -0300

    update responsive

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let texto = document.querySelector('#texto');
 let saidaDeTexto = document.querySelector('#saida-de-texto');
+let buttonCopy = document.querySelector('#button-copy');
+
+let substituicoes = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+}
 
 function criptografar(){
-    let textoEntrada = texto.value;
+    let textoEntrada = texto.value.toLowerCase();
     let textoFinal = textoEntrada.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    saidaDeTexto.innerHTML = textoFinal.replace(/a/g, '#')
-                                       .replace(/e/g, 'Z&d')
-                                       .replace(/i/g, 'fL0')
-                                       .replace(/o/g, 'Ky')
-                                       .replace(/u/g, 'ibis')
+    saidaDeTexto.innerHTML = textoFinal.replace(/[aeiou]/g, e => substituicoes[e] || e);
+
+    let botao = document.createElement('button');
+    saidaDeTexto.appendChild(botao);
+    botao.classList.add('active');
+    botao.innerHTML = 'Copiar';
 }
 
 function descriptografar(){
-    let textoEntrada = texto.value;
-    saidaDeTexto.innerHTML = textoEntrada.replace(/#/g, 'a')
-                                         .replace(/Z&d/g, 'e')
-                                         .replace(/fL0/g, 'i')
-                                         .replace(/Ky/g, 'o')
-                                         .replace(/ibis/g, 'u');
+    let textoEntrada = texto.value.toLowerCase();
+    let textoFinal = textoEntrada.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+    saidaDeTexto.innerHTML = textoFinal.replace(/ai/g, 'a')
+                                       .replace(/enter/g, 'e')
+                                       .replace(/imes/g, 'i')
+                                       .replace(/ober/g, 'o')
+                                       .replace(/ufat/g, 'u')
+
+    let botao = document.createElement('button');
+    saidaDeTexto.appendChild(botao);
+    botao.classList.add('active');
+    botao.innerHTML = 'Copiar';
 }
