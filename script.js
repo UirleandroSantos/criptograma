b2f97e9bd88779a7bcd75b087f1ebb0ecff28263
UirleandroSantos <uirleandro.santos.19@gmail.com>
2024-01-25 15:25:32 -0300

    update script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let texto = document.querySelector('#texto');
 let saidaDeTexto = document.querySelector('#saida-de-texto');
 let buttonCopy = document.querySelector('#button-copy');
 
-let substituicoes = {
+let substituicoesCriptografar = {
     'a': 'ai',
     'e': 'enter',
     'i': 'imes',
@@ -10,10 +10,18 @@ let substituicoes = {
     'u': 'ufat'
 }
 
+let substituicoesDescriptografar = {
+    'ai': 'a',
+    'enter': 'e',
+    'imes': 'i',
+    'ober': 'o',
+    'ufat': 'u'
+}
+
 function criptografar(){
     let textoEntrada = texto.value.toLowerCase();
     let textoFinal = textoEntrada.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    saidaDeTexto.innerHTML = textoFinal.replace(/[aeiou]/g, e => substituicoes[e] || e);
+    saidaDeTexto.innerHTML = textoFinal.replace(/[a|e|i|ou]/g, e => substituicoesCriptografar[e] || e);
 
     let botao = document.createElement('button');
     saidaDeTexto.appendChild(botao);
@@ -24,11 +32,7 @@ function criptografar(){
 function descriptografar(){
     let textoEntrada = texto.value.toLowerCase();
     let textoFinal = textoEntrada.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
-    saidaDeTexto.innerHTML = textoFinal.replace(/ai/g, 'a')
-                                       .replace(/enter/g, 'e')
-                                       .replace(/imes/g, 'i')
-                                       .replace(/ober/g, 'o')
-                                       .replace(/ufat/g, 'u')
+    saidaDeTexto.innerHTML = textoFinal.replace(/ai|enter|imes|ober|ufat/g, e => substituicoesDescriptografar[e] || e)
 
     let botao = document.createElement('button');
     saidaDeTexto.appendChild(botao);
